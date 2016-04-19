66f029fca82c9357f00c62ffc4a62be454dddad3
huquduy <duy.huynh@seldatinc.com>
2016-04-19 09:37:27 +0700

    env

--- config/connections.js
+++ config/connections.js
@@ -42,7 +42,7 @@ module.exports.connections = {
      ***************************************************************************/
     mysql: {
         adapter: 'sails-mysql',
-        host: '127.0.0.1',
+        host: 'localhost',
         port: 3306,
         user: 'root',
         password: '',
