d67272020ac082a9f2c927e80e56dfc1add33f02
huquduy <duy.huynh@seldatinc.com>
2016-05-09 16:23:48 +0700

    fix

--- api/policies/meetTurnOverTransfer.js
+++ api/policies/meetTurnOverTransfer.js
@@ -3,30 +3,36 @@ module.exports = function(req, res, next) {
     var ticket = req.body;
 
     if (ticket.target == 'Main Wallet') {
-    	var rollingAmount = undefined;
+        var rollingAmount = undefined;
         oldTurnOver = undefined;
 
         tokenService.parse(req)
             .then(function(user) {
-            	if(user.user_bonus_id) {
-            		UserBonus.findOne({id: user.user_bonus_id})
-            			.then(function (bonus) {
-            				rollingAmount = bonus.rolling_amount;
+                if (user.user_bonus_id) {
+                    UserBonus.findOne({ id: user.user_bonus_id })
+
+                    .then(function(bonus) {
+
+                        if (bonus.bonus_id == 1)
+                            next();
+                        else {
+
+                            rollingAmount = bonus.rolling_amount;
                             oldTurnOver = bonus.turnover;
-            				return grossApiGameService.getTurnOver(ticket.username);
-            			})
-            			.then(function (turnOvers) {
-            				if ((turnOvers.data - oldTurnOver) < rollingAmount) {
-                                var errorTurnover = ' | Turnover now is: ' + (turnOvers.data - oldTurnOver) + ' ' + user.currency;
-                                var errorRolling = ' | Rolling amount now is: ' + rollingAmount + ' ' + user.currency ;
-            					return res.json(401, {error: "Need meet Turnover to transfer back main wallet" + errorTurnover + errorRolling});
-                            }
-            				else
-            					next();
-            			})
-            	}
-                else
-                	next();
+                            grossApiGameService.getTurnOver(ticket.username)
+                            
+                                .then(function(turnOvers) {
+                                    if ((turnOvers.data - oldTurnOver) < rollingAmount) {
+                                        var errorTurnover = ' | Turnover now is: ' + (turnOvers.data - oldTurnOver) + ' ' + user.currency;
+                                        var errorRolling = ' | Rolling amount now is: ' + rollingAmount + ' ' + user.currency;
+                                        return res.json(401, { error: "Need meet Turnover to transfer back main wallet" + errorTurnover + errorRolling });
+                                    } else
+                                        next();
+                                })
+                        }
+                    })
+                } else
+                    next();
             })
 
     } else
