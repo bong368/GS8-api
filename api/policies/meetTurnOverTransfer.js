ffeb835c2f201c613a82390d6fce6aeffb91e4c4
huquduy <duy.huynh@seldatinc.com>
2016-05-23 09:48:35 +0700

    aasdf

--- api/policies/meetTurnOverTransfer.js
+++ api/policies/meetTurnOverTransfer.js
@@ -12,29 +12,32 @@ module.exports = function(req, res, next) {
                 return UserBonus.findOne({ username: creds.username })
             })
             .then(function(bonus) {
+                if (bonus) {
+                    if (bonus.bonus_id == 1)
+                        next();
+                    else {
+                        Transfers.findOne({ id: bonus.transaction_id })
+                            .then(function(transfer) {
+                                if (ticket.origin != transfer.target) {
+                                    next()
+                                } else {
+                                    rollingAmount = bonus.rolling_amount;
+                                    oldTurnOver = bonus.turnover;
+                                    grossApiGameService.getTurnOver(user.username, ticket.origin)
 
-                if (bonus.bonus_id == 1)
+                                    .then(function(turnOvers) {
+                                        if ((turnOvers.data - oldTurnOver) < rollingAmount) {
+                                            var errorTurnover = ' | Turnover now is: ' + (turnOvers.data - oldTurnOver) + ' ' + user.currency;
+                                            var errorRolling = ' | Rolling amount now is: ' + rollingAmount + ' ' + user.currency;
+                                            return res.json(401, { error: "Need meet Turnover to transfer back main wallet" + errorTurnover + errorRolling });
+                                        } else
+                                            next();
+                                    })
+                                }
+                            })
+                    }
+                } else {
                     next();
-                else {
-                    Transfers.findOne({ id: bonus.transaction_id })
-                        .then(function(transfer) {
-                            if (ticket.origin != transfer.target) {
-                                next()
-                            } else {
-                                rollingAmount = bonus.rolling_amount;
-                                oldTurnOver = bonus.turnover;
-                                grossApiGameService.getTurnOver(user.username, ticket.origin)
-
-                                .then(function(turnOvers) {
-                                    if ((turnOvers.data - oldTurnOver) < rollingAmount) {
-                                        var errorTurnover = ' | Turnover now is: ' + (turnOvers.data - oldTurnOver) + ' ' + user.currency;
-                                        var errorRolling = ' | Rolling amount now is: ' + rollingAmount + ' ' + user.currency;
-                                        return res.json(401, { error: "Need meet Turnover to transfer back main wallet" + errorTurnover + errorRolling });
-                                    } else
-                                        next();
-                                })
-                            }
-                        })
                 }
             })
 
