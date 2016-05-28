ca2459d7ea1883d7f06a880b707a8bc9bdb5b717
huquduy <duy.huynh@seldatinc.com>
2016-05-29 02:21:02 +0700

    dfd

--- api/services/allBetService.js
+++ api/services/allBetService.js
@@ -108,7 +108,7 @@ module.exports = {
     deposit: function(ticket) {
 
         var parameter = {
-            sn: (apiAllBet.propertyId.toString() + ticket.id.toString() + '00000000000000000000').substring(0, 19),
+            sn: (apiAllBet.propertyId.toString() + ticket.id.toString() + '00000000000000000000').substring(0, 20),
             client: ticket.username,
             operFlag: 1,
             credit: ticket.amount
@@ -132,7 +132,7 @@ module.exports = {
     withdrawn: function(ticket) {
 
         var parameter = {
-            sn: (apiAllBet.propertyId.toString() + ticket.id.toString() + '00000000000000000000').substring(0, 19),
+            sn: (apiAllBet.propertyId.toString() + ticket.id.toString() + '00000000000000000000').substring(0, 20),
             client: ticket.username,
             operFlag: 0,
             credit: ticket.amount
