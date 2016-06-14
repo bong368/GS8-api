d4755d104c467f5e45a28615d3cfb6b2677cc320
huquduy <duy.huynh@seldatinc.com>
2016-06-14 23:10:35 +0700

    dev

--- api/services/gamePlayService.js
+++ api/services/gamePlayService.js
@@ -22,25 +22,19 @@ module.exports = {
     signup: function(user) {
 
         var parameter = {
-            merch_id: apiGamePlay.agent,
-            merch_pwd: apiGamePlay.secret,
             cust_name: user.username,
-            cust_id: user.id,
-            currency: user.currency
+            cust_id: user.id
         }
-        apiGamePlay.url = 'https://club8api.w88.com/opcreateuser';
+        apiGamePlay.url = 'https://club8api.w88.com/op/createuser';
 
         return execGamePlayApi(parameter);
     },
 
-    anonymous: function() {
+    createAnonymous: function() {
         var parameter = {
-            merch_id: apiGamePlay.agent,
-            merch_pwd: apiGamePlay.secret,
             cust_name: 'anonymous',
             cust_id: 'anonymous',
-            test_cust: true,
-            currency: 'IDR'
+            test_cust: true
         }
         apiGamePlay.url = 'https://club8api.w88.com/op/createuser';
 
@@ -76,10 +70,7 @@ module.exports = {
         return Users.findOne({ username: username })
             .then(function(user) {
                 var parameter = {
-                    merch_id: apiGamePlay.agent,
-                    merch_pwd: apiGamePlay.secret,
                     cust_id: user.id,
-                    currency: 'IDR',
                     method: "CheckBalance"
                 }
                 apiGamePlay.url = 'https://club8api.w88.com/op/getbalance';
@@ -90,30 +81,33 @@ module.exports = {
 
     // Deposit to GamePlay
     deposit: function(ticket) {
+        return Users.findOne({ username: username })
+            .then(function(user) {
+                var parameter = {
+                    cust_id: user.id,
+                    amount: ticket.amount,
+                    trx_id: ticket.id
+                }
+                apiGamePlay.url = 'https://club8api.w88.com/op/credit';
 
-        var parameter = {
-            PlayerName: ticket.username,
-            Function: 'Deposit',
-            Amount: ticket.amount * 1000,
-            TransacID: datetimeService.getmmdd() + 'DP' + ticket.id
-        }
-        apiGamePlay.url = 'http://api.pt.gsoft88.net/VMSWservices.aspx';
-
-        return execGamePlayApi(parameter);
+                return execGamePlayApi(parameter);
+            })
     },
 
     // Withdrawn to GamePlay
     withdrawn: function(ticket) {
 
-        var parameter = {
-            PlayerName: ticket.username,
-            Function: 'Withdraw',
-            Amount: ticket.amount * 1000,
-            TransacID: datetimeService.getmmdd() + 'WD' + ticket.id
-        }
-        apiGamePlay.url = 'http://api.pt.gsoft88.net/VMSWservices.aspx';
+        return Users.findOne({ username: username })
+            .then(function(user) {
+                var parameter = {
+                    cust_id: user.id,
+                    amount: ticket.amount,
+                    trx_id: ticket.id
+                }
+                apiGamePlay.url = 'https://club8api.w88.com/op/debit';
 
-        return execGamePlayApi(parameter);
+                return execGamePlayApi(parameter);
+            })
     },
 
     // Return title of game
@@ -187,8 +181,9 @@ var execGamePlayApi = function(parameter) {
         var curl = new Curl();
         parser = new xml2js.Parser({ explicitArray: false });
         credential = {
-            secret: apiGamePlay.secret,
-            agent: apiGamePlay.agent,
+            merch_id: apiGamePlay.agent,
+            merch_pwd: apiGamePlay.secret,
+            currency: 'IDR'
         }
         parameter = _.merge(parameter, credential);
         query = '?' + queryString.stringify(parameter);
