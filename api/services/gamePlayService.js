0988619db678604e3acd0deca3f4acf025568b4f
huquduy <duy.huynh@seldatinc.com>
2016-06-17 10:31:15 +0700

    sdfsd

--- api/services/gamePlayService.js
+++ api/services/gamePlayService.js
@@ -187,8 +187,10 @@ var execGamePlayApi = function(parameter) {
             merch_pwd: apiGamePlay.secret,
             currency: 'IDR'
         }
-        if (parameter.method != 'signin')
+        if (parameter.method != 'signin') {
             parameter = _.merge(parameter, credential);
+            delete parameter.method;
+        }
         query = '?' + queryString.stringify(parameter);
         console.log(apiGamePlay.url + query);
         curl.setOpt('URL', apiGamePlay.url + query);
