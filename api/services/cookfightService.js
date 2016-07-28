193e0878be129a6daab00165927eb8ff3edf3c3b
duyhuynh <duy.huynh@seldatinc.com>
2016-07-28 16:17:39 +0700

    update

--- api/services/cookfightService.js
+++ api/services/cookfightService.js
@@ -218,7 +218,7 @@ var execplayCookfightApi = function(parameter) {
 }
 
 var adapterCurlResult = function(result, method) {
-    console.log("\n  +++ Result Cookfight " + method + " at " + date);
+    console.log("\n  +++ Result Cookfight " + method + " at " + new Date);
     console.log(result);
     switch (method) {
         case 'get_balance.aspx':
